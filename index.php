573cfabe4cc4743b1153c3440917671186622a92
TeunHSytrius <TeunHSytrius@users.noreply.github.com>
2023-11-15 13:02:57 +0100

    v1

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-require "./config/dbconn.php"; //Vereist dbconn.php dit bevat de database connectie
+session_start();
+require "./config/dbconn.php";
+include "header.php";
 
-
-?>
+if(isset($_GET["page"])) {
+    $page = $_GET["page"];
+} else {
+    $page = "login";
+}
+if ($page) {
+    include "pages/" . $page . ".php";
+}
